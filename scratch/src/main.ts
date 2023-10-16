5fcaec54afe4a84fd4f533b6c8aecc1a9c5b047f
Bista <dipendra.bista@cotiviti.com>
2023-10-16 10:36:31 +0545

    File Naming Convetion/Refactor

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,17 +1,9 @@
-import { Controller, Get, Module } from "@nestjs/common"; //More than 95% we use @nestjs/common than @nestjs/core
 import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-@Controller()
-class AppController {
-    @Get()
-    getRootRoute() {
-        return 'Hello From Nest!'
-    }
-}
 
-@Module({ controllers: [AppController] })
-class AppModule {
-}
+
+
 //used to pass some configuration while application bootstraps
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
