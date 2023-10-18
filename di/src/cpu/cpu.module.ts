51923d0283b45b5712f031d025711c17bda83db0
Bista <dipendra.bista@cotiviti.com>
2023-10-18 15:22:21 +0545

    sharing services between different modules

--- di/src/cpu/cpu.module.ts
+++ di/src/cpu/cpu.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from 'src/power/power.module';
 import { CpuService } from './cpu.service';
 
 @Module({
-  providers: [CpuService]
+  providers: [CpuService],
+  imports: [PowerModule]//Step2: import powermodule inside CpuModule
 })
 export class CpuModule { }
--- di/src/power/power.module.ts
+++ di/src/power/power.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  exports: [PowerService]//step 1:make available powerService for other modules 
+
 })
-export class PowerModule {}
+export class PowerModule { }
