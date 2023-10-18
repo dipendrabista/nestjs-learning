197c04c22ec383c683a831139d2274e486a048d5
Bista <dipendra.bista@cotiviti.com>
2023-10-18 08:48:15 +0545

    setup validationpipe for global request validation

--- messages/src/main.ts
+++ messages/src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MessegesModule } from './messeges.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(MessegesModule);
+  //validate every single requests
+  app.useGlobalPipes(
+    new ValidationPipe()
+  );
   await app.listen(4000);
 }
 bootstrap();
