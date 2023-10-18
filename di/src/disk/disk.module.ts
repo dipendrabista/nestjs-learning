4e8cd41ac18ad947e9f609406ae47185473aaa97
Bista <dipendra.bista@cotiviti.com>
2023-10-18 15:29:03 +0545

    sharing service between DiskModule and CpuModule

--- di/src/disk/disk.module.ts
+++ di/src/disk/disk.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from 'src/power/power.module';
 import { DiskService } from './disk.service';
 
 @Module({
-  providers: [DiskService]
+  providers: [DiskService],
+  imports: [PowerModule]
 })
-export class DiskModule {}
+export class DiskModule { 
+
+  
+}
