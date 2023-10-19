a118dc6aab4ee43d32a96d3941886aea0a92c7eb
dipendrabista <punojbista55@gmail.com>
2023-10-19 22:18:13 +0545

    throw NotFoundException instead of plain Error Throw!

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -3,14 +3,15 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
   Query,
 } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
-import { UsersService } from "./users.service";
 import { UpdateUserDto } from "./dtos/update-user.dto";
+import { UsersService } from "./users.service";
 
 @Controller("auth")
 export class UsersController {
@@ -21,8 +22,12 @@ export class UsersController {
   }
 
   @Get("/:id")
-  findUser(@Param("id") id: string) {
-    return this.userService.findOne(parseInt(id));
+  async findUser(@Param("id") id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException("User not found!");
+    }
+    return user;
   }
 
   @Get()
