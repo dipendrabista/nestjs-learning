8a82f695d39d9d5aa46140cf316a31bd3fb08a47
dipendrabista <punojbista55@gmail.com>
2023-10-20 15:01:44 +0545

    Custom data serialization-excluding response properties

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from "class-transformer";
 import {
   AfterInsert,
   AfterRemove,
@@ -14,6 +15,7 @@ export class User {
   @Column()
   email: string;
   @Column()
+  @Exclude()
   password: string;
 
   //hook decorator
