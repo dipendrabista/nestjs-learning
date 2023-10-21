d9d8a42604f94a1fd34a9575145106555b4a3d13
dipendrabista <punojbista55@gmail.com>
2023-10-21 10:46:25 +0545

    small fix in CurrentUserInterceptor!

--- mycv/src/users/interceptors/current-user.interceptor.ts
+++ mycv/src/users/interceptors/current-user.interceptor.ts
@@ -1,8 +1,8 @@
 import {
-    CallHandler,
-    ExecutionContext,
-    Injectable,
-    NestInterceptor,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
 } from "@nestjs/common";
 import { UsersService } from "../users.service";
 
@@ -12,7 +12,7 @@ export class CurrentUserInterceptor implements NestInterceptor {
 
   async intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+    const { userId } = request.session || {};
     if (userId) {
       const user = await this.userService.findOne(userId);
       request.CurrentUser = user;
