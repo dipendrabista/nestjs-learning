3674aeb1f6f27900cd0615f1769d9d7ed65353b8
dipendrabista <punojbista55@gmail.com>
2023-10-21 09:36:28 +0545

    install cookie-session @types/cookie-session and setup cookie session flow

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,12 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+const cookieSession = require("cookie-session");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: [],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,//it trims off extra parameter in requests
+      whitelist: true, //it trims off extra parameter in requests
     }),
   );
   await app.listen(3000);
